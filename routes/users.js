58e3afd9d37da55e60723e4be02b64f2f07fda18
王一之 <yz@ggnb.top>
2023-11-17 13:48:34 +0800

    修改用户系统

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,93 @@
-var express = require('express');
+var express = require("express");
+var request = require("request");
 var router = express.Router();
+const config = require("../config.json");
+const model = require("../models");
+const Account = model.user;
+const Record = model.record;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+function httpRequest(options) {
+  return new Promise((resolve) => {
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      resolve(JSON.parse(response.body));
+    });
+  });
+}
+
+router.get("/oauth/login", async function (req, res) {
+  // 从query中获取code
+  if (!req.query["code"]) {
+    return res.end("code is required");
+  }
+  // 通过code获取ak，再获取信息
+  const ak = await httpRequest({
+    method: "POST",
+    url: "https://bbs.tampermonkey.net.cn/plugin.php?id=codfrm_oauth2:server&op=access_token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      client_id: config.oauth.client,
+      client_secret: config.oauth.secret,
+      code: req.query["code"],
+    },
+  });
+  if (!ak.access_token) {
+    return res.end("access_token is required");
+  }
+  const userInfo = await httpRequest({
+    method: "POST",
+    url: "https://bbs.tampermonkey.net.cn/plugin.php?id=codfrm_oauth2:server&op=user",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      access_token: ak.access_token,
+    },
+  });
+  req.session.user = {
+    id: userInfo.user.uid,
+    username: userInfo.user.username,
+    nickname: userInfo.user.username,
+  };
+  // 添加记录
+  let user = await Account.findOne({
+    where: { id: userInfo.user.uid },
+  });
+  if (!user) {
+    await Account.create({
+      id: userInfo.user.uid,
+      username: userInfo.user.username,
+      nickname: userInfo.user.username,
+      passwd: "-",
+      email: "-",
+      lastlogin: new Date().valueOf() / 1000,
+    });
+  } else {
+    user.lastlogin = new Date().valueOf() / 1000;
+    user.save(user);
+  }
+  let last = req.session.last || { stage: 0, url: "/" };
+  let data = {
+    user: userInfo.user.uid,
+    url: last.url,
+    stage: last.stage,
+    pass_time: 0,
+  };
+  let record = await Record.findOne({
+    where: { stage: data.stage, user: userInfo.user.username },
+  });
+  if (!record) {
+    await Record.create(data);
+  }
+  req.session.last = { url: data.url, stage: data.stage };
+  res.redirect("/");
 });
 
 module.exports = router;
--- stages/index.js
+++ stages/index.js
@@ -9,20 +9,20 @@ module.exports = {
         <div class="puzzle">
             <div class="rule">
                 <ol>
-                    <li>这个游戏需要你安装好<a href="https://docs.scriptcat.org/" target="_blank">脚本管理器</a>才能游玩,否则你只能使用开发者工具的控制台输入命令,正常的用户点击是会被屏蔽掉的</li>
+                    <li>这个游戏需要你安装好<a href="https://docs.scriptcat.org/" target="_blank">脚本管理器</a>才能游玩,否则你只能使用开发者工具的控制台输入命令,正常的用户操作是会被屏蔽掉的</li>
                     <li>这个游戏的目标就只有一个：进入下一关。在大部份关卡中，你需要利用网页上的提示来找到密码并进入下一关。找到密码后，把密码放到网址中即可过关：${config.domain}/folder/answer.html</li>
                     <li>部分关卡的密码是填入密码框中，如果密码正确，提交网页后就会直接进入下一关。</li>
                     <li>在不少的关卡中，你需要利用 <a target="_blank" href="https://www.google.com/">Google</a> 来搜寻数据以破关。<a target="_blank" href="https://www.wikipedia.org/">Wikipedia</a> (即维基百科)里也许也有较直接的信息。</li>
-                    <li>${this.user ? '<del>' : ''}你可以<a id="login" href="/login">登录</a>以便于自动记录闯关~如果没有账号，可以<a href="/register" id="register">注册</a>一个。${this.user ? '</del>' : ''}
+                    <li>${this.user ? '<del>' : ''}你可以通过油猴中文网<a href="https://bbs.tampermonkey.net.cn/plugin.php?id=codfrm_oauth2:oauth&client_id=${config.oauth.client}&scope=user&response_type=code&redirect_uri=${encodeURIComponent(config.domain)}/oauth/login">登录</a>以便于自动记录闯关~登录即可获得油猴中文网三周年勋章，另外当你通过某些关卡后还会获得挑战者勋章哦！。${this.user ? '</del>' : ''}
                     ${this.user ? `
                     <br />欢迎你的挑战，<b><a title="个人资料" href="/u/${this.user.username}" style="color:#000">${this.user.nickname}</a></b>。
                     ${ this.laststage.stage > 0 ? `上次你已到达<a title="立即前往 >>" href="${this.laststage.url}">Stage ${this.laststage.stage}</a>。` : ''}
                     ` : ''}
                     </li>
                 </ol>
-            <p>那么，就开始吧~Let's make fun~</p>
+            <p>那么，就开始吧~Let's have fun~</p>
             </div>
-            <a href="${this.next_page}" title="Let's make fun~">
+            <a href="${this.next_page}" title="Let's have fun~">
             <img src="/images/logo.png" width="200" />
             </a>
         </div>
